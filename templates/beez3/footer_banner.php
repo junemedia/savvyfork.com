dc3db68e410cc88c973a64a48cc9e65de768df6d
John Shearer <john@ultranaut.com>
2016-03-29 21:45:07 -0400

    Update OpenX tags

--- templates/beez3/footer_banner.php
+++ templates/beez3/footer_banner.php
@@ -10,25 +10,25 @@
 	 * 'https://ox-d.junemedia.com/...'
 	 */ -->
 	 
-	 <script type="text/javascript">
-        var LB_Params = {ybot_slot:"LB", ybot_size:"", ybot_cpm:""};
-        try{
-            LB_Params = yieldbot.getSlotCriteria('LB');
-        }catch(e){/*ignore*/}
-    </script>
-
-	<div id="537228211_728x90BTF" style="width:728px;height:90px;margin:0;padding:0">
-	  <noscript><iframe id="6c66e23c15" name="6c66e23c15" src="http://ox-d.junemedia.com/w/1.0/afr?auid=537228211&cb=INSERT_RANDOM_NUMBER_HERE" frameborder="0" scrolling="no" width="728" height="90"><a href="http://ox-d.junemedia.com/w/1.0/rc?cs=6c66e23c15&cb=INSERT_RANDOM_NUMBER_HERE" ><img src="http://ox-d.junemedia.com/w/1.0/ai?auid=537228211&cs=6c66e23c15&cb=INSERT_RANDOM_NUMBER_HERE" border="0" alt=""></a></iframe></noscript>
-	</div>
 	<script type="text/javascript">
-	  var OX_ads = OX_ads || [];
-	  OX_ads.push({
-		 slot_id: "537228211_728x90BTF",
-		 auid: "537228211",
-		 vars: {"ybot_slot":LB_Params.ybot_slot, "ybot_size": LB_Params.ybot_size, "ybot_cpm": LB_Params.ybot_cpm}
-	  });
-	</script>
+    var LB_Params = {ybot_slot:"LB", ybot_size:"", ybot_cpm:""};
+    try{
+        LB_Params = yieldbot.getSlotCriteria('LB');
+    }catch(e){/*ignore*/}
+  </script>
 
-	<script type="text/javascript" src="http://ox-d.junemedia.com/w/1.0/jstag"></script>
+  <div id="537228211_728x90BTF" style="width:728px;height:90px;margin:0;padding:0">
+    <noscript><iframe id="c3c89203fd" name="c3c89203fd" src="//junemedia-d.openx.net/w/1.0/afr?auid=537228211&cb=INSERT_RANDOM_NUMBER_HERE" frameborder="0" scrolling="no" width="728" height="90"><a href="//junemedia-d.openx.net/w/1.0/rc?cs=c3c89203fd&cb=INSERT_RANDOM_NUMBER_HERE" ><img src="//junemedia-d.openx.net/w/1.0/ai?auid=537228211&cs=c3c89203fd&cb=INSERT_RANDOM_NUMBER_HERE" border="0" alt=""></a></iframe></noscript>
+  </div>
+  <script type="text/javascript">
+    var OX_ads = OX_ads || [];
+    OX_ads.push({
+      slot_id: "537228211_728x90BTF",
+      auid: "537228211",
+      vars: {"ybot_slot":LB_Params.ybot_slot, "ybot_size": LB_Params.ybot_size, "ybot_cpm": LB_Params.ybot_cpm}
+    });
+  </script>
+
+  <script type="text/javascript" src="//junemedia-d.openx.net/w/1.0/jstag"></script>
 	<!--  end openx -->
 </div>
