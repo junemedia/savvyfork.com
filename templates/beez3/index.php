fd7305e6e745358299ba36277e8604e031bb4823
John Shearer <john@ultranaut.com>
2016-03-30 09:15:22 -0400

    Add Swoop code

--- templates/beez3/index.php
+++ templates/beez3/index.php
@@ -554,6 +554,31 @@ if($menu->getActive() != $menu->getDefault() && !in_array($view,$viewArray))
 
 <!-- LiveRamp --><iframe name="_rlcdn" width=0 height=0 frameborder=0 src="http://rc.rlcdn.com/381139.html"></iframe><!-- LiveRamp -->
 
+<!-- +SWOOP -->
+<script type="text/javascript">
+  (function addSwoopOnce(domain) {
+    var win = window;
+    try {
+      while (!(win.parent == win || !win.parent.document)) {
+        win = win.parent;
+      }
+    } catch (e) {
+      /* noop */
+    }
+    var doc = win.document;
+    if (!doc.getElementById('swoop_sdk')) {
+      var serverbase = doc.location.protocol + '//ardrone.swoop.com/';
+      var s = doc.createElement('script');
+      s.type = "text/javascript";
+      s.src = serverbase + 'js/spxw.js';
+      s.id = 'swoop_sdk';
+      s.setAttribute('data-domain', domain);
+      s.setAttribute('data-serverbase', serverbase);
+      doc.head.appendChild(s);
+    }
+  })('SW-10152718-6');
+</script>
+<!-- -SWOOP -->
 
 </body>
 </html>
