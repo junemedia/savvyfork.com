b52602cd59dd42c1d38d9971cbe2d6cb645399ab
John Shearer <john@ultranaut.com>
2017-05-08 10:57:25 -0400

    Replace OpenX tags with Defy media

--- templates/beez3/partials/ads/300x250atf.php
+++ templates/beez3/partials/ads/300x250atf.php
@@ -1,27 +1,4 @@
-<div align="left" style="padding-left:20px;">
-	<!--/* OpenX Asynchronous JavaScript tag */-->
-	 
-	<script type="text/javascript">
-	var MR_Params = {ybot_slot:"MR", ybot_size:"", ybot_cpm:""};
-	try{
-		MR_Params = yieldbot.getSlotCriteria('MR');
-	}catch(e){/*ignore*/}
-	</script> 
-
-  <div id="537228210_300x250ATF" style="width:300px;height:250px;margin:0;padding:0">
-    <noscript><iframe id="52c1823e42" name="52c1823e42" src="//junemedia-d.openx.net/w/1.0/afr?auid=537228210&cb=INSERT_RANDOM_NUMBER_HERE" frameborder="0" scrolling="no" width="300" height="250"><a href="//junemedia-d.openx.net/w/1.0/rc?cs=52c1823e42&cb=INSERT_RANDOM_NUMBER_HERE" ><img src="//junemedia-d.openx.net/w/1.0/ai?auid=537228210&cs=52c1823e42&cb=INSERT_RANDOM_NUMBER_HERE" border="0" alt=""></a></iframe></noscript>
-  </div>
-  <script type="text/javascript">
-    var OX_ads = OX_ads || [];
-    OX_ads.push({
-      slot_id: "537228210_300x250ATF",
-      auid: "537228210",
-      vars: {"ybot_slot":MR_Params.ybot_slot, "ybot_size": MR_Params.ybot_size, "ybot_cpm": MR_Params.ybot_cpm}
-    });
-  </script>
-
-  <script type="text/javascript" src="//junemedia-d.openx.net/w/1.0/jstag"></script>
-
-	<!-- end openx -->
+<div align="left" style="width:300px;height:250px;padding-left:20px;">
+  <script type="text/javascript" data-placement="11320576" src="//pubportal.brkmd.com/tms/dtv4af09b1e6088bfcc403934a6315d1d992.js"></script>
 </div>
 
