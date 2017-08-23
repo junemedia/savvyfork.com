3ee69af942827c2e999dcbe6221277d101a97428
John Shearer <john@ultranaut.com>
2017-08-23 11:41:06 -0400

    Revert Defy Media ad units back to OpenX
    Undoing commit b52602c

--- templates/beez3/partials/ads/300x250btf.php
+++ templates/beez3/partials/ads/300x250btf.php
@@ -1,4 +1,26 @@
-<div align="left" style="width:300px;height:250px;padding-left:20px;margin-top:10px;">
-  <script type="text/javascript" data-placement="11320575" src="//pubportal.brkmd.com/tms/dtv45caf8cf0a651b7be367a24c053794373.js"></script>
+<div align="left" style="padding-left:20px;margin-top:10px;">
+	<!--/* OpenX Asynchronous JavaScript tag */-->
+
+  <script type="text/javascript">
+    var MR_BTF_Params = {ybot_slot:"MR_BTF", ybot_size:"", ybot_cpm:""};
+    try{
+      MR_BTF_Params = yieldbot.getSlotCriteria('MR_BTF');
+    }catch(e){/*ignore*/}
+	</script> 
+
+  <div id="537228208_300x250BTF" style="width:300px;height:250px;margin:0;padding:0">
+    <noscript><iframe id="65f6327551" name="65f6327551" src="//junemedia-d.openx.net/w/1.0/afr?auid=537228208&cb=INSERT_RANDOM_NUMBER_HERE" frameborder="0" scrolling="no" width="300" height="250"><a href="//junemedia-d.openx.net/w/1.0/rc?cs=65f6327551&cb=INSERT_RANDOM_NUMBER_HERE" ><img src="//junemedia-d.openx.net/w/1.0/ai?auid=537228208&cs=65f6327551&cb=INSERT_RANDOM_NUMBER_HERE" border="0" alt=""></a></iframe></noscript>
+  </div>
+  <script type="text/javascript">
+    var OX_ads = OX_ads || [];
+    OX_ads.push({
+      slot_id: "537228208_300x250BTF",
+      auid: "537228208",
+      vars: {"ybot_slot":MR_BTF_Params.ybot_slot, "ybot_size": MR_BTF_Params.ybot_size, "ybot_cpm": MR_BTF_Params.ybot_cpm}
+    });
+  </script>
+
+  <script type="text/javascript" src="//junemedia-d.openx.net/w/1.0/jstag"></script>
+	<!-- end openx -->
 </div>
 
--- templates/beez3/partials/ads/footer_banner.php
+++ templates/beez3/partials/ads/footer_banner.php
@@ -1,3 +1,34 @@
 <div style="text-align: center;clear: both;">
-  <script type="text/javascript" data-placement="11320577" src="//pubportal.brkmd.com/tms/dtv4d557aad7a6ccd72b16b1460918ed5c0f.js"></script>
+<!-- Javascript tag  -->
+<!--/* OpenX Asynchronous JavaScript tag */-->
+
+	<!-- /*
+	 * The tag in this template has been generated for use on a
+	 * non-SSL page. If this tag is to be placed on an SSL page, change the
+	 * 'http://ox-d.junemedia.com/...'
+	 * to
+	 * 'https://ox-d.junemedia.com/...'
+	 */ -->
+	 
+	<script type="text/javascript">
+    var LB_Params = {ybot_slot:"LB", ybot_size:"", ybot_cpm:""};
+    try{
+        LB_Params = yieldbot.getSlotCriteria('LB');
+    }catch(e){/*ignore*/}
+  </script>
+
+  <div id="537228211_728x90BTF" style="width:728px;height:90px;margin:0;padding:0">
+    <noscript><iframe id="c3c89203fd" name="c3c89203fd" src="//junemedia-d.openx.net/w/1.0/afr?auid=537228211&cb=INSERT_RANDOM_NUMBER_HERE" frameborder="0" scrolling="no" width="728" height="90"><a href="//junemedia-d.openx.net/w/1.0/rc?cs=c3c89203fd&cb=INSERT_RANDOM_NUMBER_HERE" ><img src="//junemedia-d.openx.net/w/1.0/ai?auid=537228211&cs=c3c89203fd&cb=INSERT_RANDOM_NUMBER_HERE" border="0" alt=""></a></iframe></noscript>
+  </div>
+  <script type="text/javascript">
+    var OX_ads = OX_ads || [];
+    OX_ads.push({
+      slot_id: "537228211_728x90BTF",
+      auid: "537228211",
+      vars: {"ybot_slot":LB_Params.ybot_slot, "ybot_size": LB_Params.ybot_size, "ybot_cpm": LB_Params.ybot_cpm}
+    });
+  </script>
+
+  <script type="text/javascript" src="//junemedia-d.openx.net/w/1.0/jstag"></script>
+	<!--  end openx -->
 </div>
