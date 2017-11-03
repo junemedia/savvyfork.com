c717c1c42d7d3e43cfbd0b9b210a64ac08442abf
John Shearer <john@ultranaut.com>
2017-11-03 12:51:16 -0400

    Break Swoop and Tynt ad units out into partials

--- components/com_yoorecipe/views/recipe/tmpl/recipe.php
+++ components/com_yoorecipe/views/recipe/tmpl/recipe.php
@@ -190,14 +190,7 @@ function pluralTosingular($word) {
   });
 </script>
 
-<!-- BEGIN Tynt Script -->
-<script type="text/javascript">
-  if(document.location.protocol=='http:'){
-  var Tynt=Tynt||[];Tynt.push('cJujl2gy0r5j-Tacwqm_6r');
-  (function(){var s=document.createElement('script');s.async="async";s.type="text/javascript";s.src='http://tcr.tynt.com/ti.js';var h=document.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})();
-  }
-</script>
-<!-- END Tynt Script -->
+<?php include "templates/beez3/partials/ads/tynt_js.php"; ?>
 
 <?php
 if (isset($recipe) && $recipe->published && $recipe->validated) {
---
+++ templates/beez3/partials/ads/tynt_js.php
@@ -0,0 +1,9 @@
+<!-- BEGIN Tynt Script -->
+<script type="text/javascript">
+  if(document.location.protocol=='http:'){
+  var Tynt=Tynt||[];Tynt.push('cJujl2gy0r5j-Tacwqm_6r');
+  (function(){var s=document.createElement('script');s.async="async";s.type="text/javascript";s.src='http://tcr.tynt.com/ti.js';var h=document.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})();
+  }
+</script>
+<!-- END Tynt Script -->
+
